5a5bfbc2bbcf9cc48462b0808ed5a24c7b860cbf
Jannatul Ferdous <110571832+nori0227@users.noreply.github.com>
2022-09-08 00:34:53 -0400

    Update Cleaning.py

--- Scripts/Cleaning.py
+++ Scripts/Cleaning.py
@@ -28,3 +28,43 @@ df[strings_columns] = df[strings_columns].apply(lambda x: x.str.replace(' ', '_'
 df[strings_columns] = df[strings_columns].apply(lambda x: x.str.replace('[^A-Za-z0-9]+', '_')) ## regex #### remove all special characters and whitespace ' ' from column names
 list(df[strings_columns])
 
+"""" Step 6. Assess white space or special characters """
+if not ("!".isalpha() or "!".isdigit() or "!".isspace()):
+        print("It is a special character")
+    
+""" Step 7. Convert column type to correct type """
+df = df.astype("int64", errors='ignore')
+df.head()
+df.info()
+
+"""" Step 8. Look for duplicate rows and remove duplicate rows """"
+df.drop_duplicates() # Remove duplicates
+
+""" Step 9. Assess missingness (count of missing values per column)"""
+# replacing blank, empty cells with NaN
+df.replace(to_replace='', value=np.nan, inplace=True)
+df.replace(to_replace=' ', value=np.nan, inplace=True)
+df.isnull().sum() # get a count of missing values in each column
+
+""" Step 10. New data field """
+df['modality_inperson'] = (df['Learning_Modality'].apply(lambda x: 'true' if x == 'in_person' else 'false')) 
+df.to_csv('data/dataset/School_Learning_Modalities.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
